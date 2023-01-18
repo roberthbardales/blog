7c0934a29c1687f364c3821bbedb7f66765b03d5
unknown <roberthbardales@hotmail.com>
2023-01-17 20:31:45 -0500

    17 01 2023

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -22,6 +22,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
+STATIC_ROOT = BASE_DIR.child('staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
